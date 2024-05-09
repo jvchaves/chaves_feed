4d0d469d02561b86ef1ac33a737766b7f3ad6fa7
João Victor Chaves <joao.victor.chaves159@gmail.com>
2024-05-09 18:39:58 -0300

    Feat: crio componentes para o sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,21 @@
+import styles from './Sidebar.module.css';
+
+export function Sidebar(){
+  return(
+    <aside className={styles.sidebar}>
+      <img 
+        className={styles.cover} 
+        src='https://plus.unsplash.com/premium_photo-1679177183572-a4056053b8a2?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'/>
+    
+      <div className={styles.profile}>
+        <strong>João Victor Chaves</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href='#'>Editar Seu Perfil</a>
+      </footer>
+    </aside>
+  );
+
+}
