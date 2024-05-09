b748c0b528e7a9af3779c9139c6b0655c8f06a0c
João Victor Chaves <joao.victor.chaves159@gmail.com>
2024-05-09 19:51:17 -0300

    chore: ajusto componetizacao do post

--- src/Post.jsx
+++
@@ -1,8 +0,0 @@
-export function Post(props){
-  return (
-    <div>
-      <strong><p>{props.author}</p></strong>
-      <strong><p>{props.content}</p></strong>
-    </div>
-  )
-}
---
+++ src/components/Post.jsx
@@ -0,0 +1,29 @@
+import styles from './Post.module.css';
+
+export function Post(){
+  return(
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://github.com/jvchaves.png" />
+          <div className={styles.authorInfo}>
+            <strong>João Victor Chaves</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="05 de Maio ás 19:41" dateTime="09-05-2024">Publicado há 1h</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+
+        <p>👉<a href=''>jane.design/doctorcare</a></p>
+
+        <p><a href=''>#novoprojeto #nlw #rocketseat</a></p>
+      </div>
+    </article>
+  )
+}
