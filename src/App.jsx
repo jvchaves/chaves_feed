503a6f9c5c9de39b93e246791d89d0fb70655ee1
João Victor Chaves <joao.victor.chaves159@gmail.com>
2024-05-09 18:39:30 -0300

    Feat:add post ao sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
 import { Post } from './Post';
 import { Header } from './components/Header';
 import './global.css';
+import styles from './App.module.css';
+import { Sidebar } from './components/Sidebar';
+
 export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="João Victor Chaves"
-        content="loren ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos."        
-      />
-      <Post
-        author="Chavito"
-        content="I am a New Post"        
-      />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="JV Chaves"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc ultricies ultricies. Nullam nec purus nec nunc ultricies ultricies."
+          />
+          <Post 
+            author="JV Chaves Moura"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc ultricies ultricies. Nullam nec purus nec nunc ultricies ultricies."
+          />
+        </main>
+      </div>
     </div>
   )
 }
