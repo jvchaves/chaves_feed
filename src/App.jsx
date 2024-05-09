1d6744a1d6440b514c0a1a1da9d11daf30e04a64
João Victor Chaves <joao.victor.chaves159@gmail.com>
2024-05-09 16:21:39 -0300

    Feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Post } from './Post';
+import { Header } from './components/Header';
+import './styles.css';
+export function App() {
+  return (
+    <div>
+      <Header />
+      <Post
+        author="João Victor Chaves"
+        content="loren ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos."        
+      />
+      <Post
+        author="Chavito"
+        content="I am a New Post"        
+      />
+    </div>
+  )
+}
---
+++ src/Post.jsx
@@ -0,0 +1,8 @@
+export function Post(props){
+  return (
+    <div>
+      <strong><p>{props.author}</p></strong>
+      <strong><p>{props.content}</p></strong>
+    </div>
+  )
+}
