9fa1cc3674dcd87396d62caaa66ad031354c08cf
João Victor Chaves <joao.victor.chaves159@gmail.com>
2024-05-09 18:50:31 -0300

    Feat: add foto de perfil para usuario

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,6 +8,8 @@ export function Sidebar(){
         src='https://plus.unsplash.com/premium_photo-1679177183572-a4056053b8a2?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'/>
     
       <div className={styles.profile}>
+        <img className={styles.avatar} src='https://github.com/jvchaves.png' />
+
         <strong>João Victor Chaves</strong>
         <span>Web Developer</span>
       </div>
