9a7c1b99905fa67767601cd9dbe29b8749d83d70
João Victor Chaves <joao.victor.chaves159@gmail.com>
2024-05-10 15:26:26 -0300

    Chore: altero chamado para utilizar componente avatar

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,11 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css';
+import { Avatar } from './Avatar';
 
 export function Comment(){
   return(
     <div className={styles.comment}>
-      <img src="https://github.com/jvchaves.png" />
+      <Avatar hasBorder={false} src="https://github.com/jvchaves.png" />
       
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import {PencilLine} from 'phosphor-react'
 
 import styles from './Sidebar.module.css';
+import { Avatar } from './Avatar';
 
 export function Sidebar(){
   return(
@@ -10,7 +11,7 @@ export function Sidebar(){
         src='https://plus.unsplash.com/premium_photo-1679177183572-a4056053b8a2?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'/>
     
       <div className={styles.profile}>
-        <img className={styles.avatar} src='https://github.com/jvchaves.png' />
+        <Avatar src='https://github.com/jvchaves.png' />
 
         <strong>João Victor Chaves</strong>
         <span>Web Developer</span>
