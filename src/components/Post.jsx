cf8e2929e55760de6065bb7672ca2d03b7824ac0
João Victor Chaves <joao.victor.chaves159@gmail.com>
2024-05-10 11:20:06 -0300

    Feat: add botao para comentarios

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -28,6 +28,15 @@ export function Post(){
           <a href=''>#rocketseat</a>
         </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea 
+        placeholder="Deixe um comentário" />
+
+        <button type="submit">Enviar</button>
+      </form>
     </article>
   )
 }
